cc3b191ad37d591940d90b311e43b7e384e0e20b
Nicos Maris <nicos.maris@gmail.com>
2017-07-16 18:06:37 +0300

    add each commit to gh-pages

--- deploy.sh
+++ deploy.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #
-# Updates branch gh-pages
+branch=$(git rev-parse --abbrev-ref HEAD)
 rev=$(git rev-parse --short HEAD)
-
-mkdir -p _site
-cp -R . _site/
+p=_site/$branch/${rev}
+mkdir -p $p
+cp -R . $p
 git config credential.helper "store --file=.git/credentials"
 echo "https://${GH_TOKEN}:@github.com" > .git/credentials
 
-cd _site
+cd $p
 
 git init
-git config user.name "Travis"
+git config user.name "deploy.sh"
 git config user.email "deploy.sh@gmail.com"
 
 git remote add upstream "https://$GH_TOKEN@github.com/nicosmaris/front-end-ci.git"
@@ -20,5 +20,5 @@ git fetch upstream && git reset upstream/gh-pages
 touch .
 
 git add -A .
-git commit -m "rebuild pages at ${rev}"
+git commit -m "Add commit ${rev} of branch ${branch}" 
 git push -q upstream HEAD:gh-pages
