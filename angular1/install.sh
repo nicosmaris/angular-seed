5b38f539d761977149857692d7d367b1fa5e2159
Nikos Maris <nmaris@atypon.com>
2017-07-17 17:24:36 +0300

    replace npm with yarn

--- angular1/install.sh
+++ angular1/install.sh
@@ -8,5 +8,5 @@ sh -e /etc/init.d/xvfb start
 #sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 #sudo apt-get update -q
 #sudo apt-get install -q google-chrome-stable
-npm install
+yarn install
 
